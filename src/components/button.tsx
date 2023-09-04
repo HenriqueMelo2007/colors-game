6bf2385d70b70af790d40f78ac386bc6a80c96f7
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-09-04 18:14:08 -0300

    troca a cor

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,6 +3,6 @@
 export function Button ({atualizar}) {
 
   return (
-      <button onClick={atualizar} className="text-green-400 hover:bg-green-400 hover:text-white transition-colors duration-500 text-2xl">Jogar novamente</button>
+      <button onClick={atualizar} className="text-gray-800 hover:bg-gray-800 hover:text-white transition-colors duration-500 text-2xl">Jogar novamente</button>
 )
 }
